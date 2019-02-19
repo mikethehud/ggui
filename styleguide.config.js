6379e95ef368b4de1ae99dccf8ba99eb066d783d
mikethehud <mike@eben.co.nz>
2019-02-19 23:44:40 +1300

    Enable caching and google font for styleguide

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,4 +1,18 @@
 module.exports = {
     propsParser: require('react-docgen-typescript').parse,
-    webpackConfig: require('react-scripts/config/webpack.config')
+    webpackConfig: require('react-scripts/config/webpack.config'),
+    dangerouslyUpdateWebpackConfig: (c) => {
+      c.cache = false
+      return c
+    },
+    template: {
+      head: {
+        links: [
+          {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css?family=Rubik'
+          }
+        ]
+      }
+    }
 }
